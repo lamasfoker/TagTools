a5a67a2118e8d54eef30a2b3cae424e573c55fd6
Giacomo Moscardini <giacomomoscardini@gmail.com>
2018-10-31 08:32:53 +0100

    Refactoring with the use of jQuery

--- View/js/custom-script.js
+++ View/js/custom-script.js
@@ -6,117 +6,96 @@ $(document).ready(function(){
   hideTable('tag-table');
   pagination();
 
-  document.getElementById('change-table-btn').addEventListener('click', changeTable);
-  document.getElementById('tag-table').addEventListener('click', searchThisTag);
-  document.getElementById('file-table').addEventListener('click', addSelectedItem);
-  document.getElementById('selected-table').addEventListener('click', deleteSelectedItem);
-  document.getElementById('search').addEventListener('input', searchFile);
-  document.getElementById('xml-download-btn').addEventListener('click', downloadXml);
-  document.getElementById('file-download-btn').addEventListener('click', downloadFile);
+  $('#change-table-btn').click(changeTable);
+  $('#tag-table > tbody > tr').click(searchThisTag);
+  $('#file-table > tbody > tr').click(addSelectedItem);
+  $('#search').on('input', searchFile);
+  $('#xml-download-btn').click(downloadXml);
+  $('#file-download-btn').click(downloadFile);
 
 });
 
 function changeTable() {
   cleanInput();
-  let el = document.getElementById('change-table-btn').firstElementChild;
-  if (el.textContent == 'local_offer') {
-    el.innerHTML = 'photo';
+  let el = $('#change-table-btn').children().first(),
+      search = $('#search');
+  if (el.text() === 'local_offer') {
+      el.text('photo');
       hideTable('file-table');
       showTable('tag-table');
-      document.getElementById('search').removeEventListener('input', searchFile);
-      document.getElementById('search').addEventListener('input', searchTag);
+      search.off('input', searchFile);
+      search.on('input', searchTag);
       searchTag();
   }
   else {
-    el.innerHTML = 'local_offer';
+      el.text('local_offer');
       hideTable('tag-table');
       showTable('file-table');
-      document.getElementById('search').removeEventListener('input', searchTag);
-      document.getElementById('search').addEventListener('input', searchFile);
+      search.off('input', searchTag);
+      search.on('input', searchFile);
       searchFile();
   }
 }
 
-function addSelectedItem(e) {
-  if (e.target && e.target.nodeName == 'TD') {
-    var table = document.getElementById('selected-table');
-    var fileName = e.target.parentElement.getElementsByTagName('TD')[2].textContent;
-    if (table.innerHTML.indexOf(fileName) === -1) {
-        let row = table.insertRow(0);
-        let name_cell = row.insertCell(0);
-        let cross_cell = row.insertCell(1);
-        let id_cell = row.insertCell(2);
-        let tag_cell = row.insertCell(3);
-        name_cell.innerHTML = e.target.parentElement.getElementsByTagName('TD')[2].textContent;
-        cross_cell.innerHTML = '<i class="small material-icons">clear</i>';
-        id_cell.innerHTML = e.target.parentElement.getElementsByTagName('TD')[0].textContent;
-        tag_cell.innerHTML = e.target.parentElement.getElementsByTagName('TD')[3].textContent;
-
-        document.getElementById('sku-selected').style.display = 'none';
+function addSelectedItem() {
+    let table = $('#selected-table'),
+        fileName = $(this).children(':nth-child(3)').text(),
+        cross = '<i class="small material-icons cross">clear</i>',
+        fileId = $(this).children(':nth-child(1)').text(),
+        fileTag = $(this).children(':nth-child(4)').text();
+    if (!table.children(':contains('+fileName+')').length) {
+        table.append("<tr><td>"+fileName+"</td><td>"+cross+"</td><td>"+fileId+"</td><td>"+fileTag+"</td></tr>");
+        $('.cross').last().click(deleteSelectedItem);
     }
-  }
 }
 
-function deleteSelectedItem(e) {
-  if (e.target && e.target.nodeName == 'I') {
-    if (e.target.parentElement.parentElement.parentElement.childElementCount === 1) {
-        document.getElementById('sku-selected').style.display = '';
-    }
-      e.target.parentElement.parentElement.remove();
-  }
+function deleteSelectedItem() {
+    $(this).parent().parent().remove();
 }
 
-function searchThisTag(e) {
-  if (e.target && e.target.nodeName == 'TD') {
+function searchThisTag() {
+    let tag = $(this).children(':nth-child(1)').text();
     changeTable();
-    document.getElementById('search').value=e.target.textContent;
-    /// surely there is a better way
-    setTimeout(searchTagFile, 100);
-  }
+    $('#search').val(tag);
+    searchTagFile();
 }
 
 function cleanInput() {
-    document.getElementById('search').value='';
+    $('#search').val('');
 }
 
 function downloadXml() {
-    let table = document.getElementById('selected-table');
-    let rows = table.getElementsByTagName('TR');
-    let tag, name;
-    var index =0;
-    for (let i =0; i<rows.length; i++) {
-      name = rows[i].getElementsByTagName('TD')[0].textContent;
-      tag = rows[i].getElementsByTagName('TD')[3].textContent;
-      addHiddenInputXmlForm(name, tag, index);
-      index++;
-    }
-    document.getElementById('xml-download-form').submit();
-    /// surely there is a better way
-    document.getElementById('xml-download-form').innerHTML = '<input type="submit">';
+    let rows = $('#selected-table > tr'),
+        form = $('#xml-download-form'),
+        tag, name;
+    rows.each(function(index) {
+        name = $(this).children(':nth-child(1)').text();
+        tag = $(this).children(':nth-child(4)').text();
+        addHiddenInputXmlForm(name, tag, index);
+    });
+    form.submit();
+    form.html('<input type="submit">');
 }
 
 function downloadFile() {
-    let table = document.getElementById('selected-table');
-    let rows = table.getElementsByTagName('TR');
-    let id, name;
-    var index =0;
-    for (let i =0; i<rows.length; i++) {
-        name = rows[i].getElementsByTagName('TD')[0].textContent;
-        id = rows[i].getElementsByTagName('TD')[2].textContent;
+    let rows = $('#selected-table > tr'),
+        form = $('#file-download-form'),
+        id, name;
+    rows.each(function(index) {
+        name = $(this).children(':nth-child(1)').text();
+        id = $(this).children(':nth-child(3)').text();
         addHiddenInputFileForm(name, id, index);
-        index++;
-    }
-    document.getElementById('file-download-form').submit();
-    /// surely there is a better way
-    document.getElementById('file-download-form').innerHTML = '<input type="submit">';
+    });
+    form.submit();
+    form.html('<input type="submit">');
 }
 
 function addHiddenInputXmlForm(name, tag, index) {
-  document.getElementById('xml-download-form').innerHTML += '<input type="hidden" name="'+index+'" value="'+name+'&&'+tag+'">';
+  $('#xml-download-form').append('<input type="hidden" name="'+index+'" value="'+name+'&&'+tag+'">');
 }
 
 function addHiddenInputFileForm(name, id, index) {
-  document.getElementById('file-download-form').innerHTML += '<input type="hidden" name="'+index+'" value="'+name+'&&'+id+'">';
+  $('#file-download-form').append('<input type="hidden" name="'+index+'" value="'+name+'&&'+id+'">');
 }
 
 function showTable(table) {
